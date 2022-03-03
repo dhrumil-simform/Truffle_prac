593794c112756da575da6a66a1c24df41ecafc81
dhrumil-simform <dhrumildalwadisimform@gmail.com>
2022-03-03 10:35:13 +0530

    Implemented DAPP and deployed to Kovan

---
+++ src/components/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
+import Marketplace from "../abis/Marketplace.json";
+import NavBar from "./navbar";
+import Main from "./Main";
+
+class App extends Component {
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.log("Consider using metamask");
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+    const abi = Marketplace.abi;
+    const networkId = await web3.eth.net.getId();
+    const networkData = Marketplace.networks[networkId];
+    if (networkData) {
+      const marketplace = web3.eth.Contract(abi, networkData.address);
+      this.setState({ marketplace });
+      const productCount = await marketplace.methods.productCount().call();
+      this.setState({ loading: false });
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call();
+        this.setState({ products: [...this.state.products, product] });
+      }
+    } else {
+      console.log("Contract not deployed in this network");
+    }
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: "",
+      productCount: 0,
+      products: [],
+      loading: true,
+    };
+  }
+
+  createProduct = (name, price) => {
+    this.setState({ loading: true });
+    this.state.marketplace.methods
+      .createProduct(name, price)
+      .send({ from: this.state.account })
+      .once("reciept", (reciept) => {
+        this.setState({ loading: false });
+      });
+  };
+
+  purchaseProduct = (id, price) => {
+    this.setState({ loading: true });
+    this.state.marketplace.methods
+      .purchaseProduct(id)
+      .send({ from: this.state.account, value: price })
+      .once("reciept", (reciept) => {
+        this.setState({ loading: false });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex">
+              {this.state.loading ? (
+                <div id="loader" className="text-center">
+                  <p className="text-center">Loading...</p>
+                </div>
+              ) : (
+                <Main
+                  products={this.state.products}
+                  purchaseProduct={this.purchaseProduct}
+                  createProduct={this.createProduct}
+                />
+              )}
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
